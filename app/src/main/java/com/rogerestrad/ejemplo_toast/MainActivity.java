88a4c4b0562c3ff2dd6a4b85e205e494abb6e8cf
Edgar Rogelio <rogerestrad@gmail.com>
2018-04-23 10:23:26 -0500

    Initial commit

---
+++ app/src/main/java/com/rogerestrad/ejemplo_toast/MainActivity.java
@@ -0,0 +1,31 @@
+package com.rogerestrad.ejemplo_toast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    ImageButton boton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        boton=(ImageButton) findViewById(R.id.imageButton);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater inflater=getLayoutInflater();
+                View layout = inflater.inflate(R.layout.toast_layout,null);
+                Toast toast= new Toast(getApplicationContext());
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.setView(layout);
+                toast.show();
+
+            }
+        });
+    }
+}
